299d61fee1f44737eadd0e5029f8822de30cc404
Domenico Barra <domenico@adamantic.io>
2025-08-18 14:28:23 +0200

    Added (untested) IPC for windows and MacOS

--- app/engine/webview_cocoa.hpp
+++ app/engine/webview_cocoa.hpp
@@ -3,10 +3,50 @@
 
 #import <Cocoa/Cocoa.h>
 #import <WebKit/WebKit.h>
+#include "ipc_handler.hpp"
+#include <spdlog/spdlog.h>
+
+static dbr::ipc::IPCHandlerRegistry* g_ipc_registry_cocoa = nullptr;
+static WKWebView* g_webview_cocoa = nullptr;
+
+@interface ScriptMessageHandler : NSObject <WKScriptMessageHandler>
+@end
+
+@implementation ScriptMessageHandler
+- (void)userContentController:(WKUserContentController *)userContentController 
+      didReceiveScriptMessage:(WKScriptMessage *)message {
+    if (!g_ipc_registry_cocoa) return;
+    
+    NSString* messageBody = (NSString*)message.body;
+    std::string json_str = [messageBody UTF8String];
+    
+    if (spdlog::should_log(spdlog::level::debug)) {
+        spdlog::debug("Received IPC message: {}", json_str);
+    }
+    
+    dbr::ipc::IPCResponse response = g_ipc_registry_cocoa->handle_json_message(json_str);
+    
+    if (response.id.empty()) {
+        spdlog::warn("IPC response has no ID, cannot handle response properly");
+    }
+    if (spdlog::should_log(spdlog::level::debug)) {
+        spdlog::debug("IPC response: {}", response.to_json().dump());
+    }
+    
+    std::string response_json = response.to_json().dump();
+    std::string js_code = "window.dbr.ipc._handleResponse(" + response_json + ");";
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [g_webview_cocoa evaluateJavaScript:[NSString stringWithUTF8String:js_code.c_str()] 
+                           completionHandler:nil];
+    });
+}
+@end
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 @property (strong, nonatomic) NSWindow *window;
 @property (strong, nonatomic) WKWebView *webView;
+@property (strong, nonatomic) ScriptMessageHandler *messageHandler;
 @end
 
 @implementation AppDelegate
@@ -25,7 +65,18 @@
     
     // Create WebView
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+    
+    // Setup IPC if registry is available
+    if (g_ipc_registry_cocoa) {
+        self.messageHandler = [[ScriptMessageHandler alloc] init];
+        [config.userContentController addScriptMessageHandler:self.messageHandler name:@"deskbreeze"];
+        
+        // Enable developer tools
+        [config.preferences setValue:@YES forKey:@"developerExtrasEnabled"];
+    }
+    
     self.webView = [[WKWebView alloc] initWithFrame:frame configuration:config];
+    g_webview_cocoa = self.webView;
     
     // Load URL
     NSURL *url = [NSURL URLWithString:@"http://localhost:3001"];
@@ -43,8 +94,23 @@
 
 @end
 
-inline int webview_cocoa_main() {
+inline void send_message_to_webview_cocoa(const dbr::ipc::IPCMessage& msg) {
+    if (!g_webview_cocoa || !g_ipc_registry_cocoa) return;
+
+    nlohmann::json message = msg.to_json();
+    std::string json_str = message.dump();
+    std::string js_code = "window.dbr.ipc._handleMessage(" + json_str + ");";
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [g_webview_cocoa evaluateJavaScript:[NSString stringWithUTF8String:js_code.c_str()] 
+                           completionHandler:nil];
+    });
+}
+
+inline int webview_cocoa_main(dbr::ipc::IPCHandlerRegistry* ipc_registry = nullptr) {
     @autoreleasepool {
+        g_ipc_registry_cocoa = ipc_registry;
+        
         NSApplication *app = [NSApplication sharedApplication];
         [app setActivationPolicy:NSApplicationActivationPolicyRegular];
         
--- app/engine/webview_windows.hpp
+++ app/engine/webview_windows.hpp
@@ -5,14 +5,86 @@
 #include <wrl.h>
 #include <wil/com.h>
 #include "WebView2.h"
+#include "ipc_handler.hpp"
+#include <spdlog/spdlog.h>
+#include <string>
 
 using namespace Microsoft::WRL;
+
+static dbr::ipc::IPCHandlerRegistry* g_ipc_registry_windows = nullptr;
+static ICoreWebView2* g_webview_windows = nullptr;
 
 class WebViewApp {
 private:
     HWND hwnd_;
     wil::com_ptr<ICoreWebView2Controller> webViewController_;
     wil::com_ptr<ICoreWebView2> webView_;
+
+    void SetupIPC() {
+        if (!g_ipc_registry_windows || !webView_) return;
+
+        // Add support for window.webkit.messageHandlers.deskbreeze
+        webView_->AddScriptToExecuteOnDocumentCreated(
+            LR"(
+            window.webkit = window.webkit || {};
+            window.webkit.messageHandlers = window.webkit.messageHandlers || {};
+            window.webkit.messageHandlers.deskbreeze = {
+                postMessage: function(message) {
+                    window.chrome.webview.postMessage(message);
+                }
+            };
+            )",
+            nullptr
+        );
+
+        // Set up message handler for IPC
+        EventRegistrationToken token;
+        webView_->add_WebMessageReceived(
+            Callback<ICoreWebView2WebMessageReceivedEventHandler>(
+                [this](ICoreWebView2* sender, ICoreWebView2WebMessageReceivedEventArgs* args) -> HRESULT {
+                    if (!g_ipc_registry_windows) return S_OK;
+
+                    wil::unique_cotaskmem_string messageRaw;
+                    args->TryGetWebMessageAsString(&messageRaw);
+                    
+                    std::string json_str;
+                    
+                    // Convert wide string to UTF-8
+                    int len = WideCharToMultiByte(CP_UTF8, 0, messageRaw.get(), -1, nullptr, 0, nullptr, nullptr);
+                    if (len > 0) {
+                        std::vector<char> buffer(len);
+                        WideCharToMultiByte(CP_UTF8, 0, messageRaw.get(), -1, buffer.data(), len, nullptr, nullptr);
+                        json_str = std::string(buffer.data());
+                    }
+                    
+                    if (spdlog::should_log(spdlog::level::debug)) {
+                        spdlog::debug("Received IPC message: {}", json_str);
+                    }
+                    
+                    dbr::ipc::IPCResponse response = g_ipc_registry_windows->handle_json_message(json_str);
+                    
+                    if (response.id.empty()) {
+                        spdlog::warn("IPC response has no ID, cannot handle response properly");
+                    }
+                    if (spdlog::should_log(spdlog::level::debug)) {
+                        spdlog::debug("IPC response: {}", response.to_json().dump());
+                    }
+                    
+                    std::string response_json = response.to_json().dump();
+                    std::string js_code = "window.dbr.ipc._handleResponse(" + response_json + ");";
+                    
+                    // Convert UTF-8 to wide string
+                    int wlen = MultiByteToWideChar(CP_UTF8, 0, js_code.c_str(), -1, nullptr, 0);
+                    if (wlen > 0) {
+                        std::vector<wchar_t> wbuffer(wlen);
+                        MultiByteToWideChar(CP_UTF8, 0, js_code.c_str(), -1, wbuffer.data(), wlen);
+                        webView_->ExecuteScript(wbuffer.data(), nullptr);
+                    }
+                    
+                    return S_OK;
+                }
+            ).Get(), &token);
+    }
 
 public:
     static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
@@ -67,6 +139,17 @@ public:
                             [this](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
                                 webViewController_ = controller;
                                 webViewController_->get_CoreWebView2(&webView_);
+                                g_webview_windows = webView_.get();
+
+                                // Enable developer tools if IPC is available
+                                if (g_ipc_registry_windows) {
+                                    wil::com_ptr<ICoreWebView2Settings> settings;
+                                    webView_->get_Settings(&settings);
+                                    settings->put_AreDevToolsEnabled(TRUE);
+                                    
+                                    // Setup IPC bridge
+                                    SetupIPC();
+                                }
 
                                 // Set bounds
                                 RECT bounds;
@@ -95,8 +178,27 @@ public:
     }
 };
 
-inline int webview_windows_main() {
+inline void send_message_to_webview_windows(const dbr::ipc::IPCMessage& msg) {
+    if (!g_webview_windows || !g_ipc_registry_windows) return;
+
+    nlohmann::json message = msg.to_json();
+    std::string json_str = message.dump();
+    std::string js_code = "window.dbr.ipc._handleMessage(" + json_str + ");";
+
+    // Convert UTF-8 to wide string
+    int wlen = MultiByteToWideChar(CP_UTF8, 0, js_code.c_str(), -1, nullptr, 0);
+    if (wlen > 0) {
+        std::vector<wchar_t> wbuffer(wlen);
+        MultiByteToWideChar(CP_UTF8, 0, js_code.c_str(), -1, wbuffer.data(), wlen);
+        g_webview_windows->ExecuteScript(wbuffer.data(), nullptr);
+    }
+}
+
+inline int webview_windows_main(dbr::ipc::IPCHandlerRegistry* ipc_registry = nullptr) {
     CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+    
+    g_ipc_registry_windows = ipc_registry;
+    
     WebViewApp app;
     int result = app.Run();
     CoUninitialize();
